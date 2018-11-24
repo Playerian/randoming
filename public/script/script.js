95d472784a83f7f5857441a301d4fc5be709a4ae
Liwei Huang <aa583045912@gmail.com>
2018-11-24 00:20:57 +0000

    register system succesfully implemented

---
+++ public/script/script.js
@@ -0,0 +1,39 @@
+/*global $*/
+$(".userInput").keypress(function(e){
+    var txt = String.fromCharCode(e.which);
+    if(!txt.match(/[A-Za-z]/)) 
+    {
+        return false;
+    }
+    if ($(this).val().length > 12){
+        return false;
+    }
+});
+
+$('#register').click(function(e){
+
+    let input = $('.userInput').val();
+    if (input.length === 0){
+        alert('no username');
+        return false;
+    }else if (input.length < 3){
+        alert('username too short');
+        return false;
+    }else if (input.length > 12){
+        alert('username too long');
+        return false;
+    }
+    if (input.match(/[A-Za-z]/g).join('') !==  input){
+        alert('Invalid username');
+        return false;
+    }
+});
+
+console.log('script running client side successful!');
+
+function sameUser(username){
+    $('#textArea').text("This username: "+username+" has already been taken");
+    setTimeout(function(){
+        $("#textArea").text("");
+    }, 5000);
+}
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser');
 var cleanup = require('./library/cleanup');
+var sanitizer = require('sanitize')();
+var nodemailer = require('nodemailer');
 
 //my module
 var should = require('should');
@@ -25,11 +27,12 @@ function getFile(path){
 	return fs.readFileSync(path);
 }
 //contructor-ing
-function newAccount(name, password){
+function newAccount(name, password, email){
 	dataBase[name] = {};
 	let acc = dataBase[name];
 	acc.password = password;
 	acc.title = "";
+	//acc.email = email;
 }
 
 //server-ing
@@ -47,28 +50,59 @@ app.all('/', function(req, res){
     if(req.url==='/'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(getFile(path.join(__dirname, 'html', 'index.html')));
-    }else if(suffix==='css'){
+    }
+    else if (suffix==='css'){
         res.writeHead(200, {'Content-Type': 'text/css'});
         res.end(getFile(path.join(__dirname, 'public', 'css', filename)));
-    }else if (suffix === 'js'){
+    }
+    else if (suffix === 'js'){
     	res.writeHead(200, {'Content-Type': 'text/javascript'});
-        res.end(getFile(path.join(__dirname, 'public', 'javascript', filename)));
-    }else if(suffix in ['gif', 'jpeg', 'jpg', 'png']) {
+        res.end(getFile(path.join(__dirname, 'public', 'script', filename)));
+    }
+    else if (suffix in ['gif', 'jpeg', 'jpg', 'png']) {
         res.writeHead(200, {'Content-Type': 'image/'+suffix});
         res.end(getFile(path.join(__dirname, 'public', 'images', filename)));
     }
 });
-//when registering
-
-//when getting login stuff
-app.post('/login', function(req, res) {
-  console.log(req.body);
-  let dataObj = req.body;
-  console.log(dataBase);
-  let length = Object.keys(dataBase).length;
-  dataBase.name = dataObj.userInput;
-  dataBase.password = dataObj.passInput;
+//when getting page request
+app.post('/registerPage', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(getFile(path.join(__dirname, 'html', 'register.html')));
 });
+
+app.post('/loginPage', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(getFile(path.join(__dirname, 'html', 'index.html')));
+});
+//when getting register request
+app.post('/register', function(req, res) {
+    console.log(req.body);
+    let dataObj = req.body;
+    let username = dataObj.userInput;
+    let password = dataObj.passInput;
+    let email = dataObj.emailInput;
+    if (username.length === 0 || username.length < 3 || username.length > 12){
+        return;
+    }
+    if (password.length === 0){
+        return;
+    }
+    username = username.match(/[A-Za-z]/g).join("");
+    username = username.charAt(0).toUpperCase() + username.substring(1).toLowerCase();
+    if (dataBase[username] !== undefined){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(getFile(path.join(__dirname, 'html', 'register.html')));
+        res.end('<script>sameUser("'+username+'")</script>');
+        return console.log('username has been used');
+    }
+    let account = new newAccount(username, password);
+    fs.writeFile('./data.json', JSON.stringify(dataBase, null, 2) , 'utf-8', function(err){
+        if (err){
+            throw console.log('error occurs writing file to database');
+        }
+    });
+});
+//when getting login request
 
 app.listen(process.env.PORT || 5000, function (req, res) {
   console.log('app is running at port 5000');
