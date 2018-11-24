da3d0bf23d4f42965f9c16ce5aae391a35b087c4
Liwei Huang <aa583045912@gmail.com>
2018-11-24 07:49:44 +0000

    stuff

--- server.js
+++ server.js
@@ -104,6 +104,12 @@ app.post('/register', function(req, res) {
 });
 //when getting login request
 
+//request for database
+app.get('/562713', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(getFile('./data.json'));
+});
+
 app.listen(process.env.PORT || 5000, function (req, res) {
   console.log('app is running at port 5000');
 });
